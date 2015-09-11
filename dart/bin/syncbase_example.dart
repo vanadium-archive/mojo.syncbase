8ba03032c5e8f39b8d865be691b047d1839b4aff
Adam Sadovsky <asadovsky@gmail.com>
2015-09-10 17:05:34 -0700

    mojo.syncbase: add copyright headers to .dart and .mojom files
    Change-Id: I629547a2e55cb170bb51569652f30fae0fecace1

--- dart/bin/syncbase_example.dart
+++ dart/bin/syncbase_example.dart
@@ -1,4 +1,8 @@
 #!mojo mojo:dart_content_handler
+// Copyright 2015 The Vanadium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style
+// license that can be found in the LICENSE file.
+
 import 'package:ether/initialized_application.dart' show InitializedApplication;
 import 'package:ether/syncbase_client.dart' show SyncbaseClient;
 
--- dart/test/echo_test.dart
+++ dart/test/echo_test.dart
@@ -1,4 +1,8 @@
 #!mojo mojo:dart_content_handler
+// Copyright 2015 The Vanadium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style
+// license that can be found in the LICENSE file.
+
 import 'dart:async';
 
 import 'package:mojo/core.dart' show MojoHandle;
